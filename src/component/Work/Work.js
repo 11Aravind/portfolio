433c8cd2b23a755dedd9695e4dca9537f7b1b4a4
11Aravind <aravindsreenandanam11@gmail.com>
2023-07-07 20:51:07 +0530

    some work was changed

--- src/component/Work/Work.js
+++ src/component/Work/Work.js
@@ -5,11 +5,13 @@ export default function Work() {
     <>
      
       <div className="workContainer">
+
         <div className="leftImage gradient-border" id="box">
           <img src="https://www.popsci.com/uploads/2021/02/22/45CUIW6ELJFUHEDZYMKMOPBSKY.JPG?auto=webp" alt="pic" />
         </div>
         <div className="rightContent">
-<img src="picture/topmenu.png" alt="topmenu" className='dummynavImage' />    
+          <h2>Project Name</h2>
+{/* <img src="picture/topmenu.png" alt="topmenu" className='dummynavImage' />     */}
       A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.
         </div>
       </div>
